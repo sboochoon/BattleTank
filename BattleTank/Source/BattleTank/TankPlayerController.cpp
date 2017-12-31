ef85f70e96f74292c73c193417c5b7c6dd050dc5
Shaad Boochoon <shaadb@gmail.com>
2017-12-30 22:16:05 -0500

    Added landscape, tank, camera movement, UI frame, TankController and Tank c++

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlled Tank ID: %s"), *ControlledTank->GetName());
+	}
+	
+}
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
